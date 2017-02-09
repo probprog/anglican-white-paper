529232b6c8d2a60fabe0b702d7dbe7bebbb12aba
David Tolpin <david.tolpin@gmail.com>
2017-02-09 11:09:33 +0200

    multivariate regression

---
+++ experiments/programs/linearRegression.clj
@@ -0,0 +1,17 @@
+(use 'nstools.ns)
+(ns+ linearRegression
+  (:like anglican-user.program))
+
+(def xs [0 1 2 3])
+(def ys [0 1 4 6])
+
+(defquery linearRegression 
+  (let [m (sample (normal 0 2))
+        b (sample (normal 0 2))
+        sigma (sample (gamma 1 1))
+		f (fn [x] (+ (* m x) b))]
+   (loop [data (map #(-> [%1 %2]) xs ys)]
+    (when (seq data)
+     (let [[[x y] & data] data] 
+       (observe (normal (f x) sigma) y))))
+   (predict (f 4))))
---
+++ experiments/programs/multivariateRegression.clj
@@ -0,0 +1,20 @@
+(use 'nstools.ns)
+(ns+ multivariateRegression
+  (:like anglican-user.program)
+  (:require [clojure.core.matrix :as m]))
+
+(def xs (map m/array [[0. 1.] [1. 1.5] [2. 2.] [3. 2.5] [4. 3.]]))
+(def ys [0.5 2.8, 5., 7.3, 9.5])
+
+(defquery multivariateRegression 
+  (declare :ns-primitive m)
+  (let [cov (m/identity-matrix 2)
+        m (sample (mvn (m/zero-vector 2) cov))
+        e (sample (normal 0 2))
+        sigma (sample (gamma 1 1))
+		f (fn [x] (+ (m/esum (m/mmul m x)) e))]
+   (loop [data (map #(-> [%1 %2]) xs ys)]
+    (when (seq data)
+     (let [[[x y] & data] data] 
+       (observe (normal (f x) sigma) y))))
+   (predict (f (m/array [5 4])))))
