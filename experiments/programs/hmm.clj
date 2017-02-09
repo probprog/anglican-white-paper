5fe07977d8f500cbe0b5ec71589a2dba1a01fad2
David Tolpin <david.tolpin@gmail.com>
2017-02-09 12:29:30 +0200

    added hmm

---
+++ experiments/programs/hmm.clj
@@ -0,0 +1,31 @@
+(use 'nstools.ns)
+(ns+ hmm
+  (:like anglican-user.program))
+
+(def observations 
+    [0.9 0.8 0.7 0.0
+       -0.025 -5.0 -2.0 -0.1
+       0.0 0.13 0.45 6
+       0.2 0.3 -1 -1])
+
+(def init-dist (discrete [1.0 1.0 1.0]))
+
+(def trans-dists {0 (discrete [0.1 0.5 0.4])
+                  1 (discrete [0.2 0.2 0.6])
+                  2 (discrete [0.15 0.15 0.7])})
+
+(def obs-dists {0 (normal -1 1)
+                1 (normal 1 1)
+                2 (normal 0 1)})
+
+(defquery hmm 
+  (predict
+    :states
+    (reduce
+      (fn [states obs]
+          (let [state (sample (get trans-dists
+                                   (peek states)))]
+            (observe (get obs-dists state) obs)
+            (conj states state)))
+      [(sample init-dist)]
+      observations)))
